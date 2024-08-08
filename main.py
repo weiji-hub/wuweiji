80272b844ec4747085873d6ef5d805309e2d70fd
weiji-hub <239679373@qq.com>
2024-08-08 13:37:38 +0800

    前后端代码

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
+import pandas as pd
 import streamlit as st
-from langchain.memory import ConversationBufferMemory
+from utils import dataframe_agent
 
-from utils import get_chat_response
 
-st.title("💬 克隆ChatGPT")
+def create_chart(input_data, chart_type):
+    df_data = pd.DataFrame(input_data["data"], columns=input_data["columns"])
+    df_data.set_index(input_data["columns"][0], inplace=True)
+    if chart_type == "bar":
+        st.bar_chart(df_data)
+    elif chart_type == "line":
+        st.line_chart(df_data)
+    elif chart_type == "scatter":
+        st.scatter_chart(df_data)
+
+st.title("AI数据分析")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
+    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
     st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
+    data = st.file_uploader("上传你的数据文件（CSV格式）：", type="csv")
 
-if "memory" not in st.session_state:
-    st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
-    st.session_state["messages"] = [{"role": "ai",
-                                     "content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
+if data:
+    st.session_state["df"] = pd.read_csv(data)
+    with st.expander("原始数据"):
+        st.dataframe(st.session_state["df"])
 
-for message in st.session_state["messages"]:
-    st.chat_message(message["role"]).write(message["content"])
+query = st.text_area("请输入你关于以上表格的问题，或数据提取请求，或可视化要求（支持散点图、折线图、条形图）：")
+button = st.button("生成回答")
 
-prompt = st.chat_input()
-if prompt:
-    if not openai_api_key:
-        st.info("请输入你的OpenAI API Key")
-        st.stop()
-    st.session_state["messages"].append({"role": "human", "content": prompt})
-    st.chat_message("human").write(prompt)
-
+if button and not openai_api_key:
+    st.info("请输入你的OpenAI API密钥")
+if button and "df" not in st.session_state:
+    st.info("请先上传数据文件")
+if button and openai_api_key and "df" in st.session_state:
     with st.spinner("AI正在思考中，请稍等..."):
-        response = get_chat_response(prompt, st.session_state["memory"],
-                                     openai_api_key)
-    msg = {"role": "ai", "content": response}
-    st.session_state["messages"].append(msg)
-    st.chat_message("ai").write(response)
+        response_dict = dataframe_agent(openai_api_key, st.session_state["df"], query)
+        if "answer" in response_dict:
+            st.write(response_dict["answer"])
+        if "table" in response_dict:
+            st.table(pd.DataFrame(response_dict["table"]["data"],
+                                  columns=response_dict["table"]["columns"]))
+        if "bar" in response_dict:
+            create_chart(response_dict["bar"], "bar")
+        if "line" in response_dict:
+            create_chart(response_dict["line"], "line")
+        if "scatter" in response_dict:
+            create_chart(response_dict["scatter"], "scatter")
--- utils.py
+++ utils.py
@@ -1,18 +1,58 @@
-from langchain.chains import ConversationChain
+import json
 from langchain_openai import ChatOpenAI
-
-import os
-from langchain.memory import ConversationBufferMemory
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
 
-def get_chat_response(prompt, memory, openai_api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
-    chain = ConversationChain(llm=model, memory=memory)
+PROMPT_TEMPLATE = """
+你是一位数据分析助手，你的回应内容取决于用户的请求内容。
 
-    response = chain.invoke({"input": prompt})
-    return response["response"]
+1. 对于文字回答的问题，按照这样的格式回答：
+   {"answer": "<你的答案写在这里>"}
+例如：
+   {"answer": "订单量最高的产品ID是'MNWC3-067'"}
+
+2. 如果用户需要一个表格，按照这样的格式回答：
+   {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+3. 如果用户的请求适合返回条形图，按照这样的格式回答：
+   {"bar": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+
+4. 如果用户的请求适合返回折线图，按照这样的格式回答：
+   {"line": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+
+5. 如果用户的请求适合返回散点图，按照这样的格式回答：
+   {"scatter": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}}
+6.如果既要有文字回复，又要有表格或者图形回复，按照这样的格式回答：
+    {"answer": "<你的答案写在这里>"，
+      "table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}，
+      "bar": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}，
+      "line": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}，
+      "scatter": {"columns": ["A", "B", "C", ...], "data": [34, 21, 91, ...]}
+      
+      
+      
+      
+    }
+注意：我们只支持三种类型的图表："bar", "line" 和 "scatter"。
 
 
-# memory = ConversationBufferMemory(return_messages=True)
-# print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
-# print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
+请将所有输出作为JSON字符串返回。请注意要将"columns"列表和数据列表中的所有字符串都用双引号包围。
+例如：{"columns": ["Products", "Orders"], "data": [["32085Lip", 245], ["76439Eye", 178]]}
+
+你要处理的用户请求如下： 
+"""
+#base_url = "https://api.pumpkinaigc.online/v1"
+def dataframe_agent(openai_api_key, df, query):
+    model = ChatOpenAI(model="gpt-3.5-turbo",
+                       openai_api_key=openai_api_key,
+                       #openai_api_base=base_url,
+                       temperature=0)
+    agent = create_pandas_dataframe_agent(llm=model,
+                                          df=df,
+                                          agent_executor_kwargs={"handle_parsing_errors": True},
+                                          verbose=True)
+    prompt = PROMPT_TEMPLATE + query
+    response = agent.invoke({"input": prompt})
+    response_dict = json.loads(response["output"])
+    return response_dict
+
